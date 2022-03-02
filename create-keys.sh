2ac7247a37123763e03802f850ae538287170146
Antonio Murdaca <runcom@linux.com>
2022-03-02 12:08:47 +0100

    fix create-keys.sh
    Signed-off-by: Antonio Murdaca <runcom@linux.com>

--- create-keys.sh
+++ create-keys.sh
@@ -3,5 +3,5 @@
 CONTAINER_IMAGE="${CONTAINER_IMAGE:-localhost/fdo-admin-cli}"
 
 mkdir -p keys
-for i in "diun" "manufacturer" "device_ca" "owner"; do sudo podman run -v $PWD/keys:/keys:z localhost/fdo-admin-cli generate-key-and-cert $i; done
+for i in "diun" "manufacturer" "device_ca" "owner"; do sudo podman run -v $PWD/keys:/keys:z $CONTAINER_IMAGE generate-key-and-cert $i; done
 echo "sha256:$(openssl x509 -fingerprint -sha256 -noout -in keys/diun_cert.pem | cut -d"=" -f2 | sed 's/://g')" > keys/diun_pub_key_hash
